4f9774c5c257144ef07c2f047978f384ab46bbe4
Juhyung Hong <ainu0516@gmail.com>
2022-10-23 12:10:15 +0900

    second commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 function factorial(n) {
   if (n < 0) {
     throw new Error("Factorial is only defined for non-negative integers!");
-  } else if (n === 0) {
-    return 1;
-  } else {
-    return n * factorial(n - 1);
   }
+  if (n === 0) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
 }
 module.exports = factorial;
